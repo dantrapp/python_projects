c386406d4878ca4fcb41780a79bec3b925fbab57
dantrapp <dtrapp@gmail.com>
2022-02-17 18:12:27 -0800

    init

--- password_generator/main.py
+++ password_generator/main.py
@@ -5,7 +5,7 @@ symbols, numbers, alpha
 create 12 character passwords with a random combination of these characters.
 '''
 
-import random
+import random 
 
 symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+","-", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", ")"]
 alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
---
+++ readme.md
@@ -0,0 +1,3 @@
+12 Character Password Generator
+
+To use the program, simply run it and it will generate a unique, random password from a shuffled mix of 4 symbols, 4 alpha and 4 numbers. 
