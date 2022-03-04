32ce894d7fbd85e37b757fa943cf0b7b2940d355
dantrapp <dtrapp@gmail.com>
2022-03-03 21:19:50 -0800

    fix remaining equity

--- Exit Calculator (Startup Equity)/main.py
+++ Exit Calculator (Startup Equity)/main.py
@@ -1,6 +1,6 @@
 eq = 0.05 #Your Equity In Percentage (i.e. 0.05 = 5%)
-mc = 8 #Marketcap of company in billions. 
-sell = 0.25 #Percentage of shares you'd sell after lockup.
+mc = 7 #Marketcap of company in billions. 
+sell = 0.15 #Percentage of shares you'd sell after lockup.
 
 
 def cap_calc(x, y, z):
@@ -14,7 +14,7 @@ def cap_calc(x, y, z):
   roi = longterm * arr
   roi_lt = roi * .80
   remaining_equity_value = total - sold
-  remaining_equity = (eq * 100) * (0.75)
+  remaining_equity = (eq * 100) * (1 - sell)
  
   
   print(f"The Total Market Cap Of The Company Is: ${marketcap:,.2f}\n\n Your total holdings are ${total:,.2f} at {x:.0%} equity stake. \n\n If you sold {z:0.0%} of your holdings it would be worth ${sold:,.2f}.\n\n You would be left with a remaining stake of {remaining_equity}% in the company, worth ${remaining_equity_value:,.2f}\n\nYou'd have an estimated takehome of ${capgains:,.2f} after selling ${sold:,.2f} with a 51% capital gains tax. \n\nYou'd have ${longterm:,.2f} estimated takehome after selling ${sold:,.2f} with a long-term tax of 35%\n ")
