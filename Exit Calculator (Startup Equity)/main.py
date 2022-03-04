c4830155f21ecb904a7b23f3ad603e9fd03fc121
dantrapp <dtrapp@gmail.com>
2022-03-03 19:14:55 -0800

    net

--- Exit Calculator (Startup Equity)/main.py
+++ Exit Calculator (Startup Equity)/main.py
@@ -17,7 +17,7 @@ def cap_calc(x, y, z):
   
   print(f"The Total Market Cap Of The Company Is: ${marketcap:,.2f}\n\n Your total holdings are ${total:,.2f} at {x:.0%} equity stake. \n\n If you sold {z:0.0%} of your holdings it would be worth ${sold:,.2f}. \n\nYou'd have an estimated takehome of ${capgains:,.2f} after selling ${sold:,.2f} with a 51% capital gains tax. \n\nYou'd have ${longterm:,.2f} estimated takehome after selling ${sold:,.2f} with a long-term tax of 35%\n ")
 
-  print(f"Using a low-to-moderate risk investing strategy, your ROI from an inital sale of ${longterm:,.2f} (long-term) would gross ${roi:,.2f} on an annual rate of return at {arr:.0%}, and after 20% on long-term gains, a net of ${roi_lt:,.2f} per year after taxes.")
+  print(f"Using a low-to-moderate risk investing strategy, your ROI from an inital net of ${longterm:,.2f} (long-term) would gross ${roi:,.2f} on an annual rate of return at {arr:.0%}, and after 20% on long-term gains, a net of ${roi_lt:,.2f} per year after taxes.")
 
 
 #RUN IT!
